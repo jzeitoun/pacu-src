16f10b247eccdb5fa91287d246d156e75517bcf4
Hyungtae Kim <dev@kim.ht>
2015-11-13 19:01:32 -0800

    more integration...

--- pacu/core/andor/feature/enum.py
+++ pacu/core/andor/feature/enum.py
@@ -1,5 +1,17 @@
 from pacu.core.andor.feature.base import BaseFeature, AbstractMeta
 
+class EnumishInt(int):
+    name = None
+    @classmethod
+    def make(cls, number, string=''):
+        self = cls(number)
+        self.name = string
+        return self
+    def __repr__(self):
+        return self.name if self.name else super(EnumishInt, self).__repr__()
+    def __str__(self):
+        return self.name if self.name else super(EnumishInt, self).__str__()
+
 class EnumMeta(AbstractMeta):
     @property
     def range(self):
@@ -9,6 +21,10 @@ class EnumMeta(AbstractMeta):
 class EnumFeature(BaseFeature):
     Meta = EnumMeta
     def __get__(self, inst, type):
-        return inst.handle.get_enumstr(self.feature) if inst else self
+        if inst:
+            enumint = inst.handle.enum(self.feature)
+            enumstr = inst.handle.get_enumstr(self.feature)
+            return EnumishInt.make(enumint, enumstr)
+        return self
     def __set__(self, inst, val):
         inst.handle.enum(self.feature, val)
