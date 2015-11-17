e8897fe8870dc12976dbf2abd430bf709f4d9c52
Hyungtae Kim <dev@kim.ht>
2015-11-16 19:47:41 -0800

    more integration...

--- pacu/core/svc/andor/handler/writer.py
+++ pacu/core/svc/andor/handler/writer.py
@@ -7,6 +7,7 @@ from tifffile import TiffWriter
 
 from pacu.util.path import Path
 from pacu.core.svc.andor.handler.base import BaseHandler
+from pacu.util.compat import str
 
 re_filename = re.compile(r'^\w+$')
 users_desktop = Path(os.path.expanduser('~'), 'Desktop')
@@ -19,17 +20,17 @@ class WriterHandler(BaseHandler):
             return 'Filename contains invalid character.'
         self.tiffpath = users_desktop.joinpath(filename).with_suffix('.tiff')
         self.metapath = users_desktop.joinpath(filename).with_suffix('.csv')
-        if self.tiffpath.isfile() or self.metapath.isfile():
-            return 'Filename already exists. Please provide new one...'
+    def ready(self):
+        if self.tiffpath.is_file() or self.metapath.is_file():
+            raise Exception('Filename already exists. Please provide new one...')
     def enter(self):
         print 'enter'
         self.tiff = TiffWriter(self.tiffpath.str, bigtiff=True)
         self.csv = self.metapath.open('w')
     def exposure_end(self, frame, ts):
         # rgba = pyplot.cm.jet(data, bytes=True)
-        print '.',
         self.tiff.save(frame)
-        self.csv.write(ts)
+        self.csv.write(u'{}\n'.format(ts))
     def exit(self):
         print 'exit'
         self.tiff.close()
