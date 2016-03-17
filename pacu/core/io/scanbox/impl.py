31c6c8845e2e6270248cd84fff43e2d231f1289f
Hyungtae Kim <dev@kim.ht>
2016-03-17 15:00:22 -0700

    sbx reader

--- pacu/core/io/scanbox/impl.py
+++ pacu/core/io/scanbox/impl.py
@@ -112,10 +112,10 @@ def conv(path, nchan=2):
     x, y, _, z = sbx.shape
     rgb = np.zeros((z, y, x, 3), dtype=sbx.io.dtype)
     if nchan == 2:
-        rgb[..., 0] = ~sbx.ch0
-        rgb[..., 1] = ~sbx.ch1
+        rgb[..., 1] = ~sbx.ch0
+        rgb[..., 0] = ~sbx.ch1
     elif nchan == 1:
-        rgb[..., 0] = ~sbx.ch0
+        rgb[..., 1] = ~sbx.ch0
     tiffpath = sbx.path.with_name('tiff').mkdir_if_none()
     dest = tiffpath.joinpath(sbx.path.name).with_suffix('.tiff')
     tifffile.imsave(dest.str, rgb)
@@ -126,13 +126,13 @@ def conv_all(path, nchan=2, ls='*.sbx'):
 
 # testpath = Path('/Volumes/Data/Recordings/scanbox-jack/Dario/test2/test2_000_002')
 
-# def combine(sbxpath):
-#     path = Path(sbxpath).parent.joinpath('tiff', 'STACK 1').ls()
-#     movie = np.concatenate([
-#         tifffile.imread(filename.str).mean(axis=0)[np.newaxis, ...]
-#         for filename in path.ls()
-#     ])
-#     tifffile.imsave(path.parent.joinpath('AVG', 'stack2.mean.tiff').str, movie)
+def combine(tiffpath, dest, ls='*.tiff'):
+    path = Path(tiffpath)
+    movie = np.concatenate([
+        tifffile.imread(filename.str).mean(axis=0)[np.newaxis, ...]
+        for filename in path.ls(ls)
+    ])
+    tifffile.imsave(path.joinpath(dest).with_suffix('.tiff').str, movie)
     # consider using just stack instead of concatenate
 
 # testpath = Path('/Volumes/Users/ht/Desktop/test2/test2_000_001')
@@ -143,3 +143,22 @@ def conv_all(path, nchan=2, ls='*.sbx'):
 
 # testpath = Path('/Volumes/Data/Recordings/scanbox-jack/Dario/tox3')
 # conv_all(testpath, nchan=2, ls='Tox3_000_*.sbx')
+
+# from pacu.util.path import Path
+# from pacu.util.path import Path
+# 
+#                                                                                                                                                                              
+# from pacu.core.io.scanbox import impl as sbx
+# sbx.conv_all('/media/Data/Recordings/scanbox-jack/Dario/Tox3', nchan=2, ls='Tox3_000_*')
+# sbx.conv_all('/media/Data/Recordings/scanbox-jack/Dario/Tox3', nchan=2, ls='Tox3_001_*')
+# get_ipython().magic(u'ed ')
+# combine('/media/Data/Recordings/scanbox-jack/Dario/Tox3/tiff', nchan='001.mean.tiff', ls='Tox3_001_*')
+# combine('/media/Data/Recordings/scanbox-jack/Dario/Tox3/tiff', '001.mean.tiff', ls='Tox3_001_*')
+# get_ipython().magic(u'ed -p')
+# combine('/media/Data/Recordings/scanbox-jack/Dario/Tox3/tiff', '001.mean.tiff', ls='Tox3_001_*')
+# get_ipython().magic(u'ed -p')
+# combine('/media/Data/Recordings/scanbox-jack/Dario/Tox3/tiff', '001.mean.tiff', ls='Tox3_001_*')
+# get_ipython().magic(u'ed -p')
+# combine('/media/Data/Recordings/scanbox-jack/Dario/Tox3/tiff', '001.mean.tiff', ls='Tox3_001_*')
+# combine('/media/Data/Recordings/scanbox-jack/Dario/Tox3/tiff', '000.mean.tiff', ls='Tox3_000_*')
+# get_ipython().magic(u'ed 0-14')
