5df11f93b1b4e474a1ac525594aa6583ca03669f
Hyungtae Kim <dev@kim.ht>
2016-05-02 13:13:34 -0700

    json library for ujson encoding fix

--- pacu/dep/json.py
+++ pacu/dep/json.py
@@ -1,19 +1,22 @@
 from __future__ import print_function
 from __future__ import absolute_import
 
-try:
-    import ujson as best
-    best.dumps(u'  ')
-except:
-    try:
-        import yajl as best
-        best.dumps(u'  ')
-    except:
+def deco(lib_dumps):
+    def fix_encoding(o):
+        if isinstance(o, unicode):
+            return o.encode('utf-8')
+        elif isinstance(o, dict):
+            return lib_dumps({key: fix_encode(val) for key, val in o.items()})
+        elif hasattr(o, '__iter__'):
+            return lib_dumps([fix_encode(e) for e in o])
+        else:
+            return o
+    def object_dumps(o):
         try:
-            import simplejson as best
-            best.dumps(u'  ')
-        except:
-            import json as best
-            best.dumps(u'  ')
+            return lib_dumps(o)
+        except OverflowError as e:
+            return lib_dumps(fix_encoding(o))
+    return object_dumps
 
-print('json library resolved', best)
+import ujson as best
+best.dumps = deco(best.dumps)
