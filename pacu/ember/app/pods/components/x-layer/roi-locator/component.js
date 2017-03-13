41a0aa4786511096dd58c6857327dd19515af308
Hyungtae Kim <dev@kim.ht>
2017-03-13 13:58:33 -0700

    new features for max project image

--- pacu/ember/app/pods/components/x-layer/roi-locator/component.js
+++ pacu/ember/app/pods/components/x-layer/roi-locator/component.js
@@ -1,8 +1,7 @@
 import Ember from 'ember';
 import computed from 'ember-computed-decorators';
 
-function makePolygon(cx, cy, nSides) {
-  const size = 16;
+function makePolygon(cx, cy, nSides, size=16) {
   const pts = [];
   pts.push({
     x: parseInt(cx + size * Math.cos(0)),
@@ -21,6 +20,12 @@ export default Ember.Component.extend({
   tagName: 'svg',
   x: null,
   y: null,
+  hide: false,
+  attributeBindings: 'style',
+  @computed('hide') style(hide) {
+    const visibility = hide ? 'hidden' : 'show';
+    return `visibility: ${visibility}`;
+  },
   mouseMove({offsetX, offsetY}) {
     this.set('x', offsetX);
     this.set('y', offsetY);
@@ -29,8 +34,48 @@ export default Ember.Component.extend({
     const polygon = this.get('polygon');
     this.get('workspace').appendROI({ polygon }).save();
   },
+  nSides: 8,
+  size: 16,
   @computed('x', 'y') pointAvailable(x, y) { return x && y; },
-  @computed('x', 'y') polygon(x, y) {
-    return makePolygon(x, y, 8);
+  @computed('x', 'y', 'nSides', 'size') polygon(x, y, n, s) {
+    return makePolygon(x, y, n, s);
+  },
+  didInsertElement(){
+    Mousetrap.bind('esc', e => {
+      const stream = this.get('stream');
+      stream.indexChanged();
+    });
+    Mousetrap.bind('tab', e => {
+      e.preventDefault();
+      this.toggleProperty('hide');
+    });
+    Mousetrap.bind('up', e => {
+      e.preventDefault();
+      if (this.get('nSides') > 16) return;
+      this.incrementProperty('nSides');
+    });
+    Mousetrap.bind('down', e => {
+      e.preventDefault();
+      if (this.get('nSides') < 4) return;
+      this.decrementProperty('nSides');
+    });
+    Mousetrap.bind('right', e => {
+      e.preventDefault();
+      if (this.get('size') > 64) return;
+      this.incrementProperty('size');
+    });
+    Mousetrap.bind('left', e => {
+      e.preventDefault();
+      if (this.get('size') < 4) return;
+      this.decrementProperty('size');
+    });
+  },
+  willDestroyElement(){
+    Mousetrap.unbind('esc');
+    Mousetrap.unbind('tab');
+    Mousetrap.unbind('up');
+    Mousetrap.unbind('down');
+    Mousetrap.unbind('right');
+    Mousetrap.unbind('left');
   }
 });
