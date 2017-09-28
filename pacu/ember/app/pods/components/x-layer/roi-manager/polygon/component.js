ac4bc6f6464b016657878205d111c9e2bb31d480
jzeitoun <jzeitoun@uci.edu>
2017-09-28 09:02:11 -0700

    fixing merge conflict

--- pacu/ember/app/pods/components/x-layer/roi-manager/polygon/component.js
+++ pacu/ember/app/pods/components/x-layer/roi-manager/polygon/component.js
@@ -8,11 +8,7 @@ export default Ember.Component.extend({
   classNameBindings: ['roi.active'],
   attributeBindings: ['points', 'style'],
   @computed('color') style(c) {
-<<<<<<< HEAD:pacu-dev/ember/app/pods/components/x-layer/roi-manager/polygon/component.js
-    return Ember.String.htmlSafe(`stroke: ${c};`);
-=======
-    return Ember.String.htmlSafe(`fill: ${c}; fill-opacity: 0.1;`);
->>>>>>> 3d769ec818cfd2999c03001b6b3f82cff90de516:pacu/ember/app/pods/components/x-layer/roi-manager/polygon/component.js
+    return Ember.String.htmlSafe(`fill: ${c}; fill-opacity: 0;`);
   },
   @computed('roi.polygon.@each.{x,y}') points(pg=[]) {
     return pg.map(p => `${p.x},${p.y}`).join(' ');
